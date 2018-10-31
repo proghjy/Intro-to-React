42035944720bb79d0f8828eb161fc328d17daa3f
proghjy <proghjy@gmail.com>
2018-10-31 20:50:56 +0900

    Setup for the Tutorial

--- src/index.js
+++ src/index.js
@@ -2,22 +2,66 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class ShoppingList extends React.Component {
+class Square extends React.Component {
     render() {
         return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
+            <button className="squear">
+            {
+                /* TODO */
+            }
+            </button>
+        )
+    }
+}
+
+class Board extends React.Component {
+    rednerSquare(i) {
+        return <Square/>
+    }
+
+    render() {
+        const status = 'Next player X'
+
+        return (
+            <div>
+                <div className='status'>{status}</div>
+                <div className='board-row'>
+                    {this.rednerSquare(0)}
+                    {this.rednerSquare(1)}
+                    {this.rednerSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {this.rednerSquare(3)}
+                    {this.rednerSquare(4)}
+                    {this.rednerSquare(5)}
+                </div>
+                <div className='board-row'>
+                    {this.rednerSquare(6)}
+                    {this.rednerSquare(7)}
+                    {this.rednerSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <ShoppingList/>,
+    <Game />,
     document.getElementById('root')
 )
